8744f53f3e639e1dfd0cf3a4cebef5334916ed9d
William Cai <jcwc@google.com>
2020-10-30 15:32:07 -0700

    Create a ci-test_release.sh script to run during release step
    Change-Id: Iad2bfd91c49103e7ac7a0ec843a9056998c64979

---
+++ bootstrap/tests/scripts/run_blueprints_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# To run all tests:
+# ./run_blueprints_test.sh
+
+# To run a single test:
+# ./run_blueprints_test.sh TestNetworking # Name of go test function
+
+# Only run this if in prow w/ GOOGLE_APPLICATION_CREDENTIALS
+if [ -n "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]; then
+  gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+fi
+
+gcloud components update --quiet
+gcloud components install pkg --quiet
+gcloud components install alpha --quiet
+
+cd $(dirname "${BASH_SOURCE}")/.. # Goes to bootstrap/tests directory
+
+if [ -z "${1:-}" ]; then
+  go test ./... -timeout 0
+else
+  go test -run ${1} -timeout 0
+fi

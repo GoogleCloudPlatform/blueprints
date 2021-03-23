3ca546df55dfaf0fe3643917cc60a4000d7a9b78
bharathkkb <bharathkrishnakb@gmail.com>
2021-03-23 08:54:39 -0500

    fix: remove force create folder in drs test
    Change-Id: Ie2d853b5bcb334297f222a0f825fd23fe43f21e8

--- bootstrap/tests/scripts/setup_drs_policy.sh
+++ bootstrap/tests/scripts/setup_drs_policy.sh
@@ -2,14 +2,14 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
 
 ORG_ID=$1
 DIRECTORY_CUSTOMER_ID=$(gcloud organizations list --filter "name=organizations/${ORG_ID}" --format "value(owner.directoryCustomerId)")
 
 ROOT_DIR=$(dirname "${BASH_SOURCE}")/../../..
 
-mkdir -p ./landing-zone/policies/drs
-cp -rf ${ROOT_DIR}/blueprints/policies/drs/ ./landing-zone/policies/drs
+cp -rf ${ROOT_DIR}/blueprints/policies/drs/ landing-zone/policies/drs/
 
 kpt cfg set landing-zone/policies/drs org-id ${ORG_ID}
 kpt cfg set landing-zone/policies/drs customer-ids-to-allow ${DIRECTORY_CUSTOMER_ID}

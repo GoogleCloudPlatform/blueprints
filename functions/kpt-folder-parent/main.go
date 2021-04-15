cefce91fb9dc2850a9e8e7a0165a3e834f5dd321
bharathkkb <bharathkrishnakb@gmail.com>
2021-04-14 22:30:55 -0500

    feat: update kpt-folder-parent to inject native ref
    Change-Id: I17876b3cb71273f3f81aa9d007100543e6702609

--- functions/kpt-folder-parent/main.go
+++ functions/kpt-folder-parent/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"hash/fnv"
 	"os"
-	"sort"
-	"strconv"
-	"strings"
-	"text/template"
 
 	"sigs.k8s.io/kustomize/kyaml/fn/framework"
 	"sigs.k8s.io/kustomize/kyaml/kio/filters"
@@ -19,13 +13,7 @@ import (
 const annotation string = "cnrm.cloud.google.com/folder-ref"
 const operableVersion string = "resourcemanager.cnrm.cloud.google.com/v1beta1"
 
-var (
-	operableKindSet         = map[string]bool{"Folder": true, "Project": true}
-	parsedRoleTemplate      = template.Must(template.New("role-template").Parse(rbacRoleTemplate))
-	parsedBindingTemplate   = template.Must(template.New("binding-template").Parse(rbacBindingTemplate))
-	parsedFutureTemplate    = template.Must(template.New("future-obj").Parse(futureObjecTemplate))
-	parsedReferenceTemplate = template.Must(template.New("fieldref-obj").Parse(fieldRefTemplate))
-)
+var operableKindSet = map[string]bool{"Folder": true, "Project": true}
 
 // TODO: update kyaml and use yaml.NameMeta
 type namespacedName struct {
@@ -39,51 +27,21 @@ func isOperableKind(kind string) bool {
 }
 
 func main() {
-	resourceList := &framework.ResourceList{}
-	cmd := framework.Command(resourceList, func() error {
-		var refObjs []*yaml.RNode
-		var rbacObjs []*yaml.RNode
+	p := framework.ResourceListProcessorFunc(func(resourceList *framework.ResourceList) error {
 		// Namespace: List(Operable Object Names)
-		namespaceSet := make(map[string][]string)
 		for i := range resourceList.Items {
 			if !shouldRun(resourceList.Items[i]) {
 				continue
 			}
-
-			// Build new fieldref
-			generatedRef, err := fieldReference(resourceList.Items[i])
+			// Replace with new reference style
+			newRef, err := newReference(resourceList.Items[i])
 			if err != nil {
 				return err
 			}
-			refObjs = append(refObjs, generatedRef)
-			// Append this operable object's name to the namespace's list of objects.
-			referentNamespace := mustNamespace(resourceList.Items[i])
-			targetReference := mustParseAnnotation(resourceList.Items[i])
-			if targetReference.Namespace != "" && targetReference.Namespace != referentNamespace {
-				// Add both the target AND the source name. This avoids collision in distinct runs if two different directories
-				// refer to the same target in a namespace with no local resources (b/176816568).
-				namespaceSet[targetReference.Namespace] = append(namespaceSet[targetReference.Namespace], targetReference.Name)
-				namespaceSet[targetReference.Namespace] = append(namespaceSet[targetReference.Namespace], mustName(resourceList.Items[i]))
-			}
-			namespaceSet[referentNamespace] = append(namespaceSet[referentNamespace], mustName(resourceList.Items[i]))
-
-			// Replace with a future
-			future, err := wrapInCork(resourceList.Items[i])
-			if err != nil {
-				return err
-			}
-			resourceList.Items[i] = future
+			resourceList.Items[i] = newRef
 		}
-		// add generated fieldrefs
-		resourceList.Items = append(resourceList.Items, refObjs...)
 
-		// Add RBAC objects
-		rbacObjs, err := rbacObjects(namespaceSet)
-		if err != nil {
-			return err
-		}
-		resourceList.Items = append(resourceList.Items, rbacObjs...)
-
+		var err error
 		// merge the new copies with the old copies of each resource
 		resourceList.Items, err = filters.MergeFilter{}.Filter(resourceList.Items)
 		if err != nil {
@@ -96,76 +54,23 @@ func main() {
 			return err
 		}
 
+		// add fn deprecation warning
+		deprecationNotice := &framework.Result{
+			Name: "Deprecation Notice",
+			Items: []framework.ResultItem{{
+				Message:  "folder-parent fn has been deprecated in favor of native references. For more infomation visit https://cloud.google.com/config-connector/docs/reference/resource-docs/resourcemanager/folder#schema",
+				Severity: framework.Warning,
+			}},
+		}
+		resourceList.Result = deprecationNotice
+
 		return nil
 	})
 
-	if err := cmd.Execute(); err != nil {
+	if err := framework.Execute(&p, nil); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-}
-
-// mustName will panic if the provided r doesn't have a metadata.name field. This assumes the object is already validated by shouldRun
-func mustName(r *yaml.RNode) string {
-	meta, err := r.GetMeta()
-	if err != nil {
-		panic(err)
-	}
-	return meta.Name
-}
-
-func mustNamespace(r *yaml.RNode) string {
-	meta, err := r.GetMeta()
-	if err != nil {
-		panic(err)
-	}
-	return meta.ObjectMeta.Namespace
-}
-
-// mustParseAnnotation will panic if the provided r doesn't have the expected annotation in a parsable format
-func mustParseAnnotation(r *yaml.RNode) namespacedName {
-	meta, err := r.GetMeta()
-	if err != nil {
-		panic(err)
-	}
-	ps, ok := meta.Annotations[annotation]
-	if !ok {
-		panic(fmt.Errorf("missing %v annotation: %v", annotation, meta.Annotations))
-	}
-	return parseAnnotation(ps)
-}
-
-func rbacObjects(namespaces map[string][]string) ([]*yaml.RNode, error) {
-	var rbacObj []*yaml.RNode
-	for ns := range namespaces {
-		fingerprint, err := uniqueFingerprint(namespaces[ns])
-		if err != nil {
-			return nil, fmt.Errorf("failed to hash object names in ns %v.\n%v", ns, err)
-		}
-		// Role
-		buff := &bytes.Buffer{}
-		if err := parsedRoleTemplate.Execute(buff, map[string]string{"namespace": ns, "fingerprint": fingerprint}); err != nil {
-			return nil, fmt.Errorf("role template expansion error: %v", err)
-		}
-		s := buff.String()
-		rbac, err := yaml.Parse(s)
-		if err != nil {
-			return nil, fmt.Errorf("yaml parsing of template error: %v.\nHydrated: %v", err, s)
-		}
-		rbacObj = append(rbacObj, rbac)
-		// Binding
-		buff.Reset()
-		if err := parsedBindingTemplate.Execute(buff, map[string]string{"namespace": ns, "fingerprint": fingerprint}); err != nil {
-			return nil, fmt.Errorf("roleBinding template expansion error: %v", err)
-		}
-		s = buff.String()
-		binding, err := yaml.Parse(s)
-		if err != nil {
-			return nil, fmt.Errorf("yaml parsing of template error: %v.\nHydrated: %v", err, s)
-		}
-		rbacObj = append(rbacObj, binding)
-	}
-	return rbacObj, nil
 }
 
 // This generator should run IFF it's an operable kind & version with the right annotation.
@@ -185,85 +90,20 @@ func shouldRun(r *yaml.RNode) bool {
 	if err != nil {
 		return false
 	}
-	return !yaml.IsEmpty(parent)
+	return !yaml.IsEmptyMap(parent)
 }
 
-func fieldReference(r *yaml.RNode) (*yaml.RNode, error) {
+// mustParseAnnotation will panic if the provided r doesn't have the expected annotation in a parsable format
+func mustParseAnnotation(r *yaml.RNode) namespacedName {
 	meta, err := r.GetMeta()
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	workingNamespace := meta.ObjectMeta.Namespace
-	referrerName := meta.ObjectMeta.Name
-	namespacedParent := mustParseAnnotation(r)
-	// Default to local working ns if one isn't specified in the annotation.
-	if namespacedParent.Namespace == "" {
-		namespacedParent.Namespace = workingNamespace
+	ps, ok := meta.Annotations[annotation]
+	if !ok {
+		panic(fmt.Errorf("missing %v annotation: %v", annotation, meta.Annotations))
 	}
-
-	// Generate a unique name from referrer namespaced name and parent namespaced name
-	// This is necessary to prevent otherwise same-named objects from colliding in k8s.
-	uniqueRefName, err := referenceNamer(referrerName, workingNamespace, namespacedParent.Name, namespacedParent.Namespace)
-	if err != nil {
-		return nil, err
-	}
-
-	buff := &bytes.Buffer{}
-	if err := parsedReferenceTemplate.Execute(buff, map[string]string{"hashName": uniqueRefName, "namespace": workingNamespace, "parentN": namespacedParent.Name, "parentNS": namespacedParent.Namespace}); err != nil {
-		return nil, err
-	}
-	return yaml.Parse(buff.String())
-}
-
-func wrapInCork(r *yaml.RNode) (*yaml.RNode, error) {
-	meta, err := r.GetMeta()
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse metadata: %v", err)
-	}
-	ns := meta.ObjectMeta.Namespace
-	folderName := meta.ObjectMeta.Name
-
-	namespacedParent := mustParseAnnotation(r)
-	// Default to ns of referring child object if one isn't specified in the annotation.
-	if namespacedParent.Namespace == "" {
-		namespacedParent.Namespace = ns
-	}
-
-	// Set folder parent to cork var "${folder-name}"
-	// NOTE: this variable must match the name in the spec.variables of the template below
-	err = r.PipeE(yaml.Lookup("metadata", "annotations"), yaml.SetField("cnrm.cloud.google.com/folder-id", yaml.NewScalarRNode("${folder-name}")))
-	if err != nil {
-		return nil, fmt.Errorf("failed to add folder-id annotation: %v", err)
-	}
-
-	// Generate a unique name from referrer namespaced name and parent namespaced name
-	uniqueRefName, err := referenceNamer(folderName, ns, namespacedParent.Name, namespacedParent.Namespace)
-	if err != nil {
-		return nil, err
-	}
-
-	templateContext := map[string]string{"name": folderName, "namespace": ns, "hashName": uniqueRefName}
-	buff := &bytes.Buffer{}
-
-	if err := parsedFutureTemplate.Execute(buff, templateContext); err != nil {
-		return nil, fmt.Errorf("future template expansion error: %v", err)
-	}
-	s := buff.String()
-	futureNode, err := yaml.Parse(s)
-	if err != nil {
-		return nil, fmt.Errorf("yaml parsing of template error: %v.\nHydrated: %v", err, s)
-	}
-
-	// copy this Folder krm, r, to the future nested object
-	err = futureNode.PipeE(
-		yaml.LookupCreate(yaml.MappingNode, "spec"),
-		yaml.SetField("object", r))
-
-	if err != nil {
-		return nil, fmt.Errorf("nesting folder in future error: %v", err)
-	}
-
-	return futureNode, nil
+	return parseAnnotation(ps)
 }
 
 func parseAnnotation(annotation string) namespacedName {
@@ -276,85 +116,29 @@ func parseAnnotation(annotation string) namespacedName {
 	return ref
 }
 
-// referenceNamer makes a deterministic unique (hopefully) hash from parent/child namespaced names
-func referenceNamer(childName string, childNS string, parentName string, parentNS string) (string, error) {
-	return uniqueFingerprint([]string{strings.Join([]string{childName, childNS, parentName, parentNS}, "_")})
-}
-
-// uniqueFingerprint hashes an unordered list of names to return a deterministic base36 value.
-func uniqueFingerprint(nameList []string) (string, error) {
-	sort.Strings(nameList)
-	h := fnv.New32a()
-	_, e := h.Write([]byte(strings.Join(nameList, "_")))
-	if e != nil {
-		return "", e
+// newReference injects native folderRef into an RNode spec
+func newReference(r *yaml.RNode) (*yaml.RNode, error) {
+	meta, err := r.GetMeta()
+	if err != nil {
+		return nil, err
 	}
-	i := h.Sum32()
-	s := strconv.FormatInt(int64(i), 36)
-	return s, nil
+	workingNamespace := meta.ObjectMeta.Namespace
+	namespacedParent := mustParseAnnotation(r)
+	// Default to local working ns if one isn't specified in the annotation.
+	if namespacedParent.Namespace == "" {
+		namespacedParent.Namespace = workingNamespace
+	}
+	folderRefNode := yaml.NewMapRNode(
+		&map[string]string{
+			"name":      namespacedParent.Name,
+			"namespace": namespacedParent.Namespace,
+		})
+	err = r.PipeE(
+		yaml.LookupCreate(yaml.MappingNode, "spec"),
+		yaml.SetField("folderRef", folderRefNode),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
 }
-
-var futureObjecTemplate = `apiVersion: orchestration.cnrm.cloud.google.com/v1alpha1
-kind: FutureObject
-metadata:
-  name: {{ .name }}-future
-  namespace: {{ .namespace }}
-spec:
-  object: {}
-  configMapRef:
-    name: {{ .hashName }}-ref-cm
-  variables:
-  - name: folder-name
-    type: string`
-
-var fieldRefTemplate = `apiVersion: orchestration.cnrm.cloud.google.com/v1alpha1
-kind: FieldReference
-metadata:
-  name: {{ .hashName }}-ref
-  namespace: {{ .namespace }}
-spec:
-  configMapRef:
-    name: {{ .hashName }}-ref-cm
-    key: folder-name
-  jsonPath: "{$.status.name}"
-  mutators:
-  - name: substring
-    settings:
-      start: "8" # trim the 'folders/' prefix.
-  resourceRef:
-    apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
-    kind: Folder
-    name: {{ .parentN }}
-    namespace: {{ .parentNS }}`
-
-var rbacBindingTemplate = `apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: folder-ref-binding-{{ .fingerprint }}
-  namespace: {{ .namespace }}
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: Role
-  name: folder-ref-role-{{ .fingerprint }}
-subjects:
-- kind: ServiceAccount
-  name: default
-  namespace: orchestrator-system`
-
-var rbacRoleTemplate = `apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-  name: folder-ref-role-{{ .fingerprint }}
-  namespace: {{ .namespace }}
-rules:
-- apiGroups:
-  - resourcemanager.cnrm.cloud.google.com
-  resources:
-  - folders
-  - projects
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update`
--- functions/kpt-folder-parent/main_test.go
+++ functions/kpt-folder-parent/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"reflect"
 	"testing"
 
 	"sigs.k8s.io/kustomize/kyaml/yaml"
@@ -16,6 +18,19 @@ metadata:
 spec:
     displayName: nested-folder`
 
+const annotatedFolderKrmExpected string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
+kind: Folder
+metadata:
+  annotations:
+    cnrm.cloud.google.com/folder-ref: top-folder
+  name: folder-b
+  namespace: project-hierarchy
+spec:
+  displayName: nested-folder
+  folderRef:
+    name: top-folder
+    namespace: project-hierarchy`
+
 const annotatedFolderDiffNamespaceKrm string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
 kind: Folder
 metadata:
@@ -27,6 +42,20 @@ metadata:
 spec:
     displayName: nested-folder`
 
+const annotatedFolderDiffNamespaceKrmExpected string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
+kind: Folder
+metadata:
+    annotations:
+        cnrm.cloud.google.com/folder-ref: |
+           {"namespace": "namespace-two", "name": "top-folder"}
+    name: folder-b
+    namespace: project-hierarchy
+spec:
+    displayName: nested-folder
+    folderRef:
+        name: top-folder
+        namespace: namespace-two`
+
 const notAnnotatedFolderKrm string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
 kind: "Folder"
 metadata:
@@ -37,38 +66,50 @@ metadata:
 spec:
     displayName: parent-folder`
 
-const childSameParentOne string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
-kind: Folder
-metadata:
-    annotations:
-        cnrm.cloud.google.com/folder-ref: |
-            {"namespace": "ns-two", "name": "parent-obj"}
-    name: child-folder
-    namespace: ns-one
-spec:
-    displayName: nested-folder-a`
+func Test_newReference(t *testing.T) {
+	type args struct {
+		r string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"simple", args{annotatedFolderKrm}, annotatedFolderKrmExpected, false},
+		{"with-explicit-ns", args{annotatedFolderDiffNamespaceKrm}, annotatedFolderDiffNamespaceKrmExpected, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// get input RNode
+			i, err := yaml.Parse(tt.args.r)
+			if err != nil {
+				t.Errorf("Error parsing input yaml: %v", err)
+			}
+			// get expected RNode
+			expected, err := yaml.Parse(tt.want)
+			if err != nil {
+				t.Errorf("Error parsing expected yaml: %v", err)
+			}
+			got, err := newReference(i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newReference() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(marshalJSON(got), marshalJSON(expected)) {
+				t.Errorf("newReference() = %v, want %v", marshalJSON(got), marshalJSON(expected))
+			}
+		})
+	}
+}
 
-const childSameParentTwo string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
-kind: Folder
-metadata:
-    annotations:
-        cnrm.cloud.google.com/folder-ref: |
-            {"namespace": "ns-two", "name": "parent-obj"}
-    name: child-folder2
-    namespace: ns-one
-spec:
-    displayName: nested-folder-b`
-
-const childSimilarParent string = `apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
-kind: Folder
-metadata:
-    annotations:
-        cnrm.cloud.google.com/folder-ref: |
-            {"namespace": "ns-one", "name": "parent-obj"}
-    name: child-folder
-    namespace: ns-two
-spec:
-    displayName: nested-folder-b`
+func marshalJSON(r *yaml.RNode) string {
+	j, err := r.MarshalJSON()
+	if err != nil {
+		log.Fatalf("Error marshalling json: %v", err)
+	}
+	return string(j)
+}
 
 func TestShouldRun(t *testing.T) {
 	r, err := yaml.Parse(annotatedFolderKrm)
@@ -91,163 +132,6 @@ func TestShouldRun(t *testing.T) {
 	}
 }
 
-func TestFieldGen(t *testing.T) {
-	r, err := yaml.Parse(annotatedFolderKrm)
-	if err != nil {
-		t.Fatal("lol this isn't even the test yet:\n", err)
-	}
-	out, err := fieldReference(r)
-	if err != nil {
-		t.Fatal("failed generation\n", err)
-	}
-	meta, err := out.GetMeta()
-	if err != nil {
-		t.Fatal("fail to get meta\n", err)
-	}
-	if meta.Name != "1lqrf51-ref" {
-		t.Fatal("Wrong name: [", meta.Name, "] wanted [1lqrf51-ref]")
-	}
-}
-
-func TestFieldGenJson(t *testing.T) {
-	r, err := yaml.Parse(annotatedFolderDiffNamespaceKrm)
-	if err != nil {
-		t.Fatal("lol this isn't even the test yet:\n", err)
-	}
-	out, err := fieldReference(r)
-	if err != nil {
-		t.Fatal("failed generation\n", err)
-	}
-	meta, err := out.GetMeta()
-	if err != nil {
-		t.Fatal("fail to get meta\n", err)
-	}
-	if meta.Name != "1xokn1q-ref" {
-		t.Fatal("Wrong name: [", meta.Name, "] wanted [1xokn1q-ref]")
-	}
-}
-
-func TestCorkWrap(t *testing.T) {
-	r, err := yaml.Parse(annotatedFolderKrm)
-	if err != nil {
-		t.Fatal("lol this isn't even the test yet:\n", err)
-	}
-	out, e := wrapInCork(r)
-	if e != nil {
-		t.Fatal(e)
-	}
-	m, e := out.GetMeta()
-	if e != nil {
-		t.Fatal(e)
-	}
-	if m.Kind != "FutureObject" {
-		t.Fatal("Did not wrap in a future")
-	}
-}
-
-func TestCorkWrapJson(t *testing.T) {
-	r, err := yaml.Parse(annotatedFolderDiffNamespaceKrm)
-	if err != nil {
-		t.Fatal("lol this isn't even the test yet:\n", err)
-	}
-	out, e := wrapInCork(r)
-	if e != nil {
-		t.Fatal(e)
-	}
-	m, e := out.GetMeta()
-	if e != nil {
-		t.Fatal(e)
-	}
-	if m.Kind != "FutureObject" {
-		t.Fatal("Did not wrap in a future")
-	}
-}
-
-func TestRbac(t *testing.T) {
-	out, e := rbacObjects(map[string][]string{"my-namespace1": {"name1"}, "my-other-namespace": {"name2"}})
-	if e != nil {
-		t.Fatal(e)
-	}
-	if len(out) != 4 {
-		t.Fatalf("Expected 4 objects for 2 namespaces, got %v", len(out))
-	}
-	for _, oneObj := range out {
-		meta, err := oneObj.GetMeta()
-		if err != nil {
-			t.Fatal("fail to get meta\n", err)
-		}
-		if !(meta.Kind == "RoleBinding" || meta.Kind == "Role") {
-			t.Fatalf("Expected valid RBAC kind, got %v", meta.Kind)
-		}
-	}
-}
-
-func TestDeterministicFingerprint(t *testing.T) {
-	// Order should not matter for the name list.
-	testLists := [][]string{
-		{"nameOne", "another-name"},
-		{"another-name", "nameOne"},
-	}
-
-	for _, testList := range testLists {
-		fingerprintStr, e := uniqueFingerprint(testList)
-		if e != nil {
-			t.Fatal("Failed to hash testList\n", e)
-		}
-		// s0uiqu was calculated beforehand. The answer for testList should only change if the input strings change.
-		if fingerprintStr != "s0uiqu" {
-			t.Fatalf("Got [%v] Expected [s0uiqu]", fingerprintStr)
-		}
-	}
-}
-
-func TestUniqueFingerprint(t *testing.T) {
-	// Similar namesets shouldn't give the same hash
-	testLists := [][]string{
-		{"alpha", "bravo"},
-		{"alphabravo"},
-	}
-	var testResults []string
-
-	for _, testList := range testLists {
-		fingerprintStr, e := uniqueFingerprint(testList)
-		if e != nil {
-			t.Fatal("Failed to hash testList\n", e)
-		}
-		testResults = append(testResults, fingerprintStr)
-	}
-
-	if testResults[0] == testResults[1] {
-		t.Fatal("These unique contexts shouldn't have the same fingerprint!")
-	}
-}
-
-func TestDeterminismForIntegration(t *testing.T) {
-	// Test hash determinism for specific cases
-	// Element 0 is the expected hash
-	testLists := [][]string{
-		{"1ip8jcq", "outer-folder", "project-ref-other"},
-		{"g90evd", "third-folder"},
-	}
-	var testResults []string
-	var testExpected []string
-
-	for _, testList := range testLists {
-		testExpected = append(testExpected, testList[0])
-		fingerprintStr, e := uniqueFingerprint(testList[1:])
-		if e != nil {
-			t.Fatal("Failed to hash testList\n", e)
-		}
-		testResults = append(testResults, fingerprintStr)
-	}
-
-	for i := range testResults {
-		if testResults[i] != testExpected[i] {
-			t.Fatalf("Expected [%s] Got [%s]", testExpected[i], testResults[i])
-		}
-	}
-}
-
 func TestMustParseAnnotation(t *testing.T) {
 	r, err := yaml.Parse(annotatedFolderDiffNamespaceKrm)
 	if err != nil {
@@ -259,98 +143,5 @@ func TestMustParseAnnotation(t *testing.T) {
 	}
 	if ref.Namespace != "namespace-two" {
 		t.Fatalf("Got [%v] Expected [namespace-two]", ref.Namespace)
-	}
-}
-
-func TestFieldReferenceUnique(t *testing.T) {
-	// same parent 1&2 refer to the same parent from the same ns
-	// similar parent has the same identifying namespaced name strings but in slightly different arrangement
-	// none should share a reference name or collide
-	r1, err := yaml.Parse(childSameParentOne)
-	if err != nil {
-		t.Fatal("Failed to parse childSameParentOne")
-	}
-	r2, err := yaml.Parse(childSameParentTwo)
-	if err != nil {
-		t.Fatal("Failed to parse childSameParentTwo")
-	}
-	r3, err := yaml.Parse(childSimilarParent)
-	if err != nil {
-		t.Fatal("Failed to parse childSimilarParent")
-	}
-
-	ref1, err := fieldReference(r1)
-	if err != nil {
-		t.Fatal("failed generation for childSameParentOne\n", err)
-	}
-	ref2, err := fieldReference(r2)
-	if err != nil {
-		t.Fatal("failed generation for childSameParentTwo\n", err)
-	}
-	ref3, err := fieldReference(r3)
-	if err != nil {
-		t.Fatal("failed generation for childSimilarParent\n", err)
-	}
-
-	m1, err := ref1.GetMeta()
-	if err != nil {
-		t.Fatal(err)
-	}
-	m2, err := ref2.GetMeta()
-	if err != nil {
-		t.Fatal(err)
-	}
-	m3, err := ref3.GetMeta()
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Parsing done, test for (incorrect) matches.
-	if m1.Name == m2.Name {
-		t.Fatalf("Got [%v] [%v], expected unique FieldReference names.", m1.Name, m2.Name)
-	}
-	if m1.Name == m3.Name {
-		t.Fatalf("Got [%v] [%v], expected unique FieldReference names.", m1.Name, m3.Name)
-	}
-	if m3.Name == m2.Name {
-		t.Fatalf("Got [%v] [%v], expected unique FieldReference names.", m3.Name, m2.Name)
-	}
-}
-
-func TestFieldRefMatchFuture(t *testing.T) {
-	r, err := yaml.Parse(childSameParentOne)
-	if err != nil {
-		t.Fatal("Failed to parse childSameParentOne")
-	}
-	reference, err := fieldReference(r)
-	if err != nil {
-		t.Fatal("failed FieldReference generation\n", err)
-	}
-	future, err := wrapInCork(r)
-	if err != nil {
-		t.Fatal("failed FutureObject generation\n", err)
-	}
-	cName1, err := reference.Pipe(yaml.Lookup("spec", "configMapRef", "name"))
-	if err != nil {
-		t.Fatal("failed FieldReference parsing\n", err)
-	}
-	cName2, err := future.Pipe(yaml.Lookup("spec", "configMapRef", "name"))
-	if err != nil {
-		t.Fatal("failed FutureObject parsing\n", err)
-	}
-	cNameStr1 := yaml.GetValue(cName1)
-	cNameStr2 := yaml.GetValue(cName2)
-	if cNameStr1 != cNameStr2 {
-		t.Fatalf("Got [%v] [%v], expected configMapRef names should match.", cNameStr1, cNameStr2)
-	}
-}
-
-func TestNameStringSwapsUnique(t *testing.T) {
-	s1, _ := referenceNamer("foo", "bar", "baz", "qux")
-	s2, _ := referenceNamer("foo", "qux", "baz", "bar")
-	if len(s1) == 0 {
-		t.Fatal("referenceNamer note generating valid hash string")
-	}
-	if s1 == s2 {
-		t.Fatalf("Got [%v], expected unique name hashes.", s1)
 	}
 }

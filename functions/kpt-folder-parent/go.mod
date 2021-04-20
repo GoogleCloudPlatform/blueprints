4be77a2966bdb104b933feebaf2635f382433c31
bharathkkb <bharathkrishnakb@gmail.com>
2021-04-20 14:54:03 -0500

    Revert "feat: update kpt-folder-parent to inject native ref"
    This reverts commit cefce91fb9dc2850a9e8e7a0165a3e834f5dd321.
    
    Change-Id: I4bbb12f539962f504b0686e5ff84c02b26101c33

--- functions/kpt-folder-parent/go.mod
+++ functions/kpt-folder-parent/go.mod
@@ -3,14 +3,6 @@ module cnrm.googlesource.com/kpt-folder-parent
 go 1.14
 
 require (
-	github.com/go-errors/errors v1.1.1 // indirect
-	github.com/go-openapi/spec v0.20.3 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/gobuffalo/here v0.6.2 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/spf13/cobra v1.1.3 // indirect
-	github.com/xlab/treeprint v1.1.0 // indirect
-	golang.org/x/net v0.0.0-20210414194228-064579744ee0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	sigs.k8s.io/kustomize/kyaml v0.10.17
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	sigs.k8s.io/kustomize/kyaml v0.5.0
 )
--- functions/kpt-folder-parent/scripts/integ-test.sh
+++ functions/kpt-folder-parent/scripts/integ-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #  expected to be run from yakima/kpt-folder-parent
 kpt version  # sanity test
-TMPDIR=$(mktemp -d)
-OUTPUT="$(kpt fn run sample/ --enable-exec --exec-path ./bin/main --dry-run --results-dir ${TMPDIR})"
+
+OUTPUT="$(kpt fn run sample/ --enable-exec --exec-path ./bin/main --dry-run)"
 
 # TODO(b/172476714): Split out the test cases and general refactor
 
@@ -11,7 +11,7 @@ OUTPUT="$(kpt fn run sample/ --enable-exec --exec-path ./bin/main --dry-run --re
 # |- folder-b (ns 1)
 # |  |- project-c (ns 1)
 # |- folder-c (ns 2)
-#
+# 
 # folder-d (ns 3)
 # |- project-d (ns 1)
 # |- project-e (ns 1)
@@ -19,12 +19,25 @@ OUTPUT="$(kpt fn run sample/ --enable-exec --exec-path ./bin/main --dry-run --re
 # outer-folder (other-ns -- not defined in sample)
 # |- project-ref-other (ns 1)
 
-# There should be a total of 8 resources:
+# There should be a total of 18 resources:
 # 4 Folders, 4 Projects
-# 2 Folders(b,c) and 4 Projects(c,d,e,ref-ther) should have FolderRefs injected
-# There should also be a deprecation warning in results-dir
-TOTAL_RESOURCES="8"
+# 6 Cork References
+# 8 RBAC Objects, 1 role and 1 binding for 4 namespaces
+TOTAL_RESOURCES="22"
 
+# Expect these strings to be in the output.
+# These are three RBAC bindings with a deterministic hash suffix that should not change, unless the resources in sample change.
+EXPECTED=("folder-ref-binding-edd7ym" \
+"folder-ref-binding-g90evd" \
+"folder-ref-binding-f7vffq" \
+"folder-ref-binding-1ip8jcq") # 1ip8jcq is expected for other-ns, a hash composed of the referring and referred objects (see TestDeterminismForIntegration)
+
+for expect in "${EXPECTED[@]}"; do
+	if [ "$(echo "$OUTPUT" | grep -c "$expect")" == "0" ]; then
+		echo "failed	kpt tests failed: output missing expected string ["$expect"]"
+		exit 1
+	fi
+done
 
 if [ "$(echo "$OUTPUT" | grep -cE ^kind)" != "$TOTAL_RESOURCES" ]
 then
@@ -33,10 +46,23 @@ then
 	exit 1
 fi
 
-if  [ "$(echo "$OUTPUT" | grep -c Folder)" == "4" ] &&
-	[ "$(echo "$OUTPUT" | grep -c Project)" == "4" ] &&
-	[ "$(echo "$OUTPUT" | grep -c folderRef)" == "6" ] &&
-	[ "$(cat "$TMPDIR/results-0.yaml" | grep -c warning)" == "1" ]
+if [ "$(echo "$OUTPUT" | grep -c 'name: folder-ref-role-g90evd')" != "2" ]
+then
+	echo "failed	kpt tests failed: should have role and binding yaml both reference role"
+	exit 1
+fi
+
+# All configMap name references should come in pairs
+if [ "$(echo "$OUTPUT" | grep -A 1 configMapRef | grep name | sort | uniq -u)" != "" ]
+then
+	echo "failed	kpt tests failed: there is a unique configMapRef name; FieldRef and Futures are not being paired together"
+	exit 1
+fi
+
+if [ "$(echo "$OUTPUT" | grep -c FutureObject)" == "6" ] && 
+	[ "$(echo "$OUTPUT" | grep -c FieldReference)" == "6" ] &&
+	[ "$(echo "$OUTPUT" | grep -c RoleBinding)" == "4" ] &&
+	[ "$(echo "$OUTPUT" | grep -cE "^kind: Role")" == "8" ]
 then
 	echo "ok	kpt tests passed"
 	exit 0

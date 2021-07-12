fb9e86dcc067498c0b9837a1c7e1cfabe8c00c6c
Karl Isenberg <karlisenberg@google.com>
2021-07-12 15:26:46 -0700

    Change add-attribution.sh to auto-detect vars

--- utils/add-attribution.sh
+++ utils/add-attribution.sh
@@ -13,11 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o errexit -o nounset -o pipefail
 
-BLUEPRINT_NAME=$1
-BLUEPRINT_VERSION=$2
+PKG_PATH="${1:-.}"
+BLUEPRINT_NAME="$(grep -m 1 "name:" "${PKG_PATH}/Kptfile" | sed 's/[[:space:]]*name:[[:space:]]*//')"
+BLUEPRINT_VERSION="${BLUEPRINT_VERSION:-$(cd "${PKG_PATH}" && git describe --abbrev=0)}"
 
-kpt fn source . |
-  kpt cfg grep "apiVersion=cnrm" |
-  kpt fn run --image gcr.io/kpt-fn/set-annotation:v0.1 -- "cnrm.cloud.google.com/blueprint=cnrm/${BLUEPRINT_NAME}/${BLUEPRINT_VERSION}" |
-  kpt fn sink .
+kpt fn source "${PKG_PATH}" |
+    kpt cfg grep "apiVersion=cnrm" |
+    kpt fn run --image gcr.io/kpt-fn/set-annotation:v0.1 -- "cnrm.cloud.google.com/blueprint=cnrm/${BLUEPRINT_NAME}/${BLUEPRINT_VERSION}" |
+    kpt fn sink "${PKG_PATH}"

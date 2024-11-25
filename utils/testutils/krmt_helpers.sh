ae12704d5d4a9abbd0777c3c0bbf94f4baaa0807
Andrew Peabody <andrewpeabody@google.com>
2024-11-25 12:16:26 -0800

    Update krmt_helpers.sh

--- utils/testutils/krmt_helpers.sh
+++ utils/testutils/krmt_helpers.sh
@@ -96,7 +96,7 @@ create_cc(){
     CC_NAME="${CC_NAME:-testcc}"
     echo "Creating Config Controller ${CC_NAME} in ${PROJECT_ID}"
     echo "Enabling services"
-    gcloud services enable krmapihosting.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com anthos.googleapis.com --project=$PROJECT_ID
+    gcloud services enable compute.googleapis.com krmapihosting.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com anthos.googleapis.com --project=$PROJECT_ID
     sleep 60s
     echo "Creating CC"
     gcloud alpha anthos config controller create ${CC_NAME} --location=us-central1 --project=$PROJECT_ID
